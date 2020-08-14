c4768062f9ba90260df4f62158b4abf8f64f1def
Melissa C <66797764+Wednesdayw@users.noreply.github.com>
2020-08-14 10:37:20 -0400

    day 2 updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import {BASE_URL, API_KEY} from "./constants/index"
+import Footer from "./components/footer"
+import Header from "./components/header"
+import Body from "./components/body"
+
+
 import "./App.css";
 
+
+
 function App() {
+const [image, setImage] = useState('')
+
+useEffect(() => {
+  axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+  .then(res => {
+    console.log(res.data)
+    setImage(res.data)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}, [])
+
   return (
+
     <div className="App">
-      <p>
+      <Header imageInfo={image}/>
+
+      <Body image={image} />
+
+      <Footer imageInfo={image}/>
+
+      {/* <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      </p> */}
     </div>
   );
 }
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod'
+export const API_KEY = 'r799KpoHB0639MMl3AF9gQgX9HdKvnLgSdOuNfQZ'
